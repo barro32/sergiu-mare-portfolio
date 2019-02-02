b1600ea13fbbf042cad01908f2415ad7045f2d37
Sergiu Mare <mare.sergiu@yahoo.com>
2019-02-02 13:16:34 +0000

    feature/msp-07-refactore-contact-mod: email functionality;

--- server/contact/contact.php
+++ server/contact/contact.php
@@ -1,30 +1,22 @@
 <?php
-// check if fields passed are empty
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // create email body and send it
-$to = "mare.sergiu@gmail.com";
-$email_subject = "Contact form submitted by:  $name";
+$to = "contact@maresergiu.co.uk";
+$email_subject = "Form submited by: $name";
 $email_body = "You have received a new message. \n\n".
-				  "Here are the details:\n \nName: $name \n ".
-              "Email: $email_address\n Message \n $message";
-              "Phone: $phone\n Message \n $phone";
-$headers = "From: info@maresergiu.co.uk\n";
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+              "Here are the details:\n\n".
+              "Name: $name \n\n".
+              "Email: $email_address \n\n".
+              "Phone: $phone \n\n".
+              "Message: $message \n\n";
+$headers = "From: $to";	
 
-header("Location: http://www.maresergiu.co.uk/#contact-me"); /* Redirect browser */
+// send the email
+(mail($to,$email_subject,$email_body,$headers)); // send email
+header("Location: https://www.maresergiu.co.uk/#contact-me"); /* Redirect browser */
 exit();
 ?>
